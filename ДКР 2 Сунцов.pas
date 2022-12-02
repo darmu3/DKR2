3ca8659b769761c8f2718883f72f3b572f6616c4
darmu3 <117808824+darmu3@users.noreply.github.com>
2022-12-02 18:41:13 +0300

    Update ДКР 2 Сунцов.pas

--- ДКР 2 Сунцов.pas
+++ ДКР 2 Сунцов.pas
@@ -1,14 +1,20 @@
-﻿begin
+begin
   var a: array[1..100] of integer;
-  var min, n: integer;
+  var b, e, n, d, min: integer;
   print('Размер массива от 1 до 100, введите n = ');
   readln(n);   
-  if (n < 1) and (n > 100) then exit;
+  if (n < 1) or (n > 100) then exit;
   print('Введите массив А:');
   for var i := 1 to n do read(a[i]);
   writeln;
-  min := 100;
-  for var i := 1 to n do
-    if abs(a[i]) < abs(min) then min := a[i];
-  print('Минимум по модулю = ', abs(min));
+  if abs(a[1])>abs(a[2])then min:=abs(a[1])-abs(a[2]) else min:=abs(a[2])-abs(a[1]);
+  for var j:=1 to n do
+    for var i:=1 to n do
+    if (min>abs(a[i])-abs(a[j])) and (0<=abs(a[i])-abs(a[j])) and (a[i]<>a[j])then
+      begin
+      min:=abs(a[i])-abs(a[j]);
+      b:=a[i];
+      e:=a[j];
+       end;
+  print( b, ' и ', e);
 end.
